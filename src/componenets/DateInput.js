79786f372958b820cbc585de1f9b265e407e5353
Austin Moore <37962463+Austinmoore1492@users.noreply.github.com>
2019-10-04 11:17:27 -0500

    Fixed Mobile DatePicker Issue

--- src/componenets/DateInput.js
+++ src/componenets/DateInput.js
@@ -1,25 +1,18 @@
-import React from "react";
-import PropTypes from "prop-types";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import React from 'react';
+import PropTypes from 'prop-types';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const DateInput = props => (
   <div
     style={{
-      textAlign: "center",
-      fontSize: "1.2rem",
-      marginBottom: "1rem",
-      padding: "5px"
+      textAlign: 'center',
+      fontSize: '1.2rem',
+      marginBottom: '1rem',
+      padding: '5px'
     }}
   >
-    <p
-      style={{
-        padding: "5px",
-        display: "inline-block"
-      }}
-    >
-      Select a Date:
-    </p>
+    <p className="dateSelect">Select a Date:</p>
     <DatePicker selected={props.date} onChange={props.changeDate} />
   </div>
 );
